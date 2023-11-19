548d3b52d85b85da943b624fa84272b094949164
Erick <erickungu82@gmail.com>
2023-11-19 10:40:58 +0300

    Add function disabilityChecked() This functionc checks if the disability check is checked

--- script.js
+++ script.js
@@ -4,104 +4,85 @@ const resetSelection = document.querySelector('.reset-checkbox')
 form = document.querySelector('.form')
 
 
-function calculateLevy(){
+function calculateLevy() {
     let housingLevy = 0.015 * grossPay.value
     return housingLevy
 }
 
-function calculateNHIF(){
+function calculateNHIF() {
     let basePay = grossPay.value
     let nhif;
 
-    if (basePay <= 5999){
+    if (basePay <= 5999) {
         nhif = 150;
-    }
-    else if(basePay >= 6000 && basePay <= 7999){
+    } else if (basePay >= 6000 && basePay <= 7999) {
         nhif = 300;
-    }
-    else if(basePay >= 8000 && basePay <= 11999){
+    } else if (basePay >= 8000 && basePay <= 11999) {
         nhif = 400;
-    }
-    else if(basePay >= 12000 && basePay <= 14999){
+    } else if (basePay >= 12000 && basePay <= 14999) {
         nhif = 500;
-    }
-    else if(basePay >= 15000 && basePay <= 19999){
+    } else if (basePay >= 15000 && basePay <= 19999) {
         nhif = 600;
-    }
-    else if(basePay >= 20000 && basePay <= 24999){
+    } else if (basePay >= 20000 && basePay <= 24999) {
         nhif = 750;
-    }
-    else if(basePay >= 25000 && basePay <= 29999){
+    } else if (basePay >= 25000 && basePay <= 29999) {
         nhif = 850;
-    }
-    else if(basePay >= 30000 && basePay <= 34999){
+    } else if (basePay >= 30000 && basePay <= 34999) {
         nhif = 900;
-    }
-    else if(basePay >= 35000 && basePay <= 39999){
+    } else if (basePay >= 35000 && basePay <= 39999) {
         nhif = 950;
-    }
-    else if(basePay >= 40000 && basePay <= 44999){
+    } else if (basePay >= 40000 && basePay <= 44999) {
         nhif = 1000;
-    }
-    else if(basePay >= 45000 && basePay <= 49999){
+    } else if (basePay >= 45000 && basePay <= 49999) {
         nhif = 1100;
-    }
-    else if(basePay >= 50000 && basePay <= 59999){
+    } else if (basePay >= 50000 && basePay <= 59999) {
         nhif = 1200;
-    }
-    else if(basePay >= 60000 && basePay <= 69999){
+    } else if (basePay >= 60000 && basePay <= 69999) {
         nhif = 1300;
-    }
-    else if(basePay >= 70000 && basePay <= 79999){
+    } else if (basePay >= 70000 && basePay <= 79999) {
         nhif = 1400;
-    }
-    else if(basePay >= 80000 && basePay <= 89999){
+    } else if (basePay >= 80000 && basePay <= 89999) {
         nhif = 1500;
-    }
-    else if(basePay >= 90000 && basePay <= 99999){
+    } else if (basePay >= 90000 && basePay <= 99999) {
         nhif = 1600;
-    }
-    else{
+    } else {
         nhif = 1700;
     }
 
     return nhif;
 }
 
-function calculateNSSF(){
+function calculateNSSF() {
     let nssfTier1 = document.querySelector('.nssf-tier1');
     let nssfTier1_2 = document.querySelector('.nssf-tier1-2')
     let nssf;
     let error = document.querySelector('.error')
-    if(!error){
+    if (!error) {
         error = document.createElement('p')
         error.classList.add('error')
     }
-    
-    if(nssfTier1.checked == true && nssfTier1_2.checked == true){
+
+    if (nssfTier1.checked == true && nssfTier1_2.checked == true) {
         error.innerHTML = 'Please select only one'
         form.appendChild(error);
-    }
-    else if(nssfTier1.checked){
+    } else if (nssfTier1.checked) {
         nssf = 360;
-    }
-    else if (nssfTier1_2.checked){
+    } else if (nssfTier1_2.checked) {
         nssf = 1080;
-    }
-    else{
+    } else {
         error.innerHTML = 'Select one of the two tiers'
         form.appendChild(error);
     }
 
-    resetSelection.addEventListener('click',function(){
+    resetSelection.addEventListener('click', function() {
         document.querySelectorAll('input[type="radio"]').forEach(input => input.checked = false)
     })
 
-    
+
     return nssf
 }
 
-function calculateIncomeTax(){
+function calculateIncomeTax() {
     let nssfDeductable = calculateNSSF()
     let taxableIncome = grossPay.value - nssfDeductable
     let nhifRelief = calculateNHIF() * 0.15
@@ -109,37 +90,37 @@ function calculateIncomeTax(){
     let tax = 0;
 
     if (taxableIncome <= 24000) {
-      tax = taxableIncome * 0.10;
+        tax = taxableIncome * 0.10;
     } else if (taxableIncome <= 32333) {
-      tax = 24000 * 0.10 + (taxableIncome - 24000) * 0.25;
+        tax = 24000 * 0.10 + (taxableIncome - 24000) * 0.25;
     } else if (taxableIncome <= 500000) {
-      tax = 24000 * 0.10 + 8333 * 0.25 + (taxableIncome - 32333) * 0.30;
+        tax = 24000 * 0.10 + 8333 * 0.25 + (taxableIncome - 32333) * 0.30;
     } else if (taxableIncome <= 800000) {
-      tax =
-        24000 * 0.10 +
-        8333 * 0.25 +
-        467667 * 0.30 +
-        (taxableIncome - 500000) * 0.325;
+        tax =
+            24000 * 0.10 +
+            8333 * 0.25 +
+            467667 * 0.30 +
+            (taxableIncome - 500000) * 0.325;
     } else {
-      tax =
-        24000 * 0.10 +
-        8333 * 0.25 +
-        467667 * 0.30 +
-        300000 * 0.325 +
-        (taxableIncome - 800000) * 0.35;
+        tax =
+            24000 * 0.10 +
+            8333 * 0.25 +
+            467667 * 0.30 +
+            300000 * 0.325 +
+            (taxableIncome - 800000) * 0.35;
     }
-  
+
     // Subtract personal relief
     tax -= (2400 + nhifRelief);
-  
+
     // Ensure tax is non-negative
     tax = Math.max(tax, 0);
-    
+
     return tax.toFixed(2)
-    
+
 }
 
-function calculateNetPay(){
+function calculateNetPay() {
     let incomeTax = parseFloat(calculateIncomeTax());
     let deductableNSSF = parseFloat(calculateNSSF());
     let deductableNHIF = parseFloat(calculateNHIF());
@@ -147,12 +128,21 @@ function calculateNetPay(){
 
     let totalDeductions = incomeTax + deductableLevy + deductableNSSF + deductableNHIF
 
-    let netPay = grossPay.value - totalDeductions 
-    
+    let netPay = grossPay.value - totalDeductions
+
     return netPay
 }
 
-calculate.addEventListener('click', function(){
+function disabilityCheck() {
+    const disabilityCheck = document.querySelector('.disability-check')
+    if (disabilityCheck.checked) {
+        return true
+    } else {
+        return false
+    }
+}
+
+calculate.addEventListener('click', function() {
     // let deductableLevy = calculateLevy()
     // let netPay = grossPay.value - deductableLevy
 
@@ -161,5 +151,5 @@ calculate.addEventListener('click', function(){
     // console.log(calculateNSSF())
     // console.log(calculateIncomeTax())
     console.log(calculateNetPay())
-} )
 
+})
